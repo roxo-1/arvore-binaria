26cc3c8bf8596e18d0ac31d0995d98c48ce48a72
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-08-27 10:44:37 -0300

    COMMIT "árvore inserção e percurso inordem"

---
+++ Arvore.java
@@ -0,0 +1,38 @@
+public class Arvore{
+    No raiz; // fazer o nó depois
+    public Arvore () { raiz = null; }
+    public void inserir(int valor){
+        if(raiz == null){
+            raiz = new No(valor);
+        }else{
+            No p, ant;
+            p = raiz; ant = raiz;
+            while (p!=null){
+                ant = p;
+                if(valor<chave){
+                    p=p.esquerda;
+                }else if(valor > chave){
+                    p=p.direita
+                }else{
+                    System.out.println("O valor já existe");
+                    return;
+                }
+            }
+            if(valor < ant.chave){
+                ant.esquerda=novoNo(valor);
+            } else{
+                ant.direita=novoNo(valor);
+            }
+        }
+    }
+
+    private void mostraInOrdem(){
+        if (no != null) {
+        mostraInOrdem(no.left);
+        System.out.print(no.value + " "); 
+        mostraInOrdem(no.right);
+    }
+    void mostrar(){
+        mostraInOrdem();
+    }
+}
